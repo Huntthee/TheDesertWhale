27c81b23b27fa2d3c74821b517d6319d9736900d
Huntthee <mikeallison000@gmail.com>
2021-07-15 22:58:00 -0600

    added content to policy page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,12 @@
 // A little bit of increased UX. Pre-populate the form text-field with some default text about the customer's choice, depending on which link they've clicked!
-
 const addValue = choice => {
   const reply = "I would like to get the ";
   const pick = choice;
   document.getElementById("comments").value += `${reply}${pick}`;
-}
+};
 
 
-// The code below should assign an event listener to the font awesome icon and allow the user to toggle the hamburger menu on and off.
+// Simple logic for a toggle function
 const toggleMenu = () => {
   let menu = document.getElementById("main-nav");
   if (menu.style.display === "none") {
@@ -16,5 +15,3 @@ const toggleMenu = () => {
     menu.style.display = "none";
   }
 };
-
-document.getElementById('bars').addEventListener("click", toggleMenu)
