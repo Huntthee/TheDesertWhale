5da369f5c03f82649c6dce074597dd4eb8517d91
Huntthee <mikeallison000@gmail.com>
2021-08-03 15:33:37 -0600

    began large overhaul, converted file system to ejs

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv/config');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res){
+  res.render('pages/index');
+});
+
+app.get('/portfolio', function(req, res){
+  res.render('pages/portfolio');
+});
+
+app.get('/commissions', function(req, res){
+  res.render('pages/commissions');
+});
+
+app.get('/about', function(req, res){
+  res.render('pages/about');
+});
+
+app.get('/policy', function(req, res){
+  res.render('pages/policy');
+});
+
+const port = process.env.PORT || '8080';
+app.listen(port, err => {
+  if (err)
+    throw err
+  console.log(`Server listening on ${port}`)
+});
+console.log('Server is listening');
